166744885541b2856024877250d7f321d510901e
Abal Bohora <themailofabal@gmail.com>
2026-05-17 14:51:43 +0545

    Fix database connection error

--- src/main/java/com/artopia1/utils/DBConnection.java
+++ src/main/java/com/artopia1/utils/DBConnection.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// Utility class for managing database connections
+
 public class DBConnection {
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "1234";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/artopia";
+
+    // Method to establish and return a database connection
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
